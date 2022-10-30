e275d03b10d6c60e45cbf7afc1da043d5cc42e32
5calV <110572069+5calV@users.noreply.github.com>
2022-10-30 17:58:40 +0100

    updated window

--- tkinter_pw_gen.py
+++ tkinter_pw_gen.py
@@ -17,10 +17,10 @@ import pyperclip
 
 # Global Variables
 
-root = Tk()
-root.title('calV s Password Generator')
-root.geometry("900x500")
-root.configure(bg='#303135')
+window = Tk()
+window.title('calV s Password Generator')
+window.geometry("900x500")
+window.configure(bg='#303135')
 passwrd = StringVar()
 passlen = IntVar()
 passlen.set(0)
@@ -54,12 +54,12 @@ def copytoclipboard():
 
 # Text
  
-Label(root, text="Password Generator", font="Courier 30 italic").pack()
-Label(root, text="made by calV", font="Courier 15 bold").pack()
-Label(root, text="Enter the number of places for your Password").pack(pady=3)
-Entry(root, textvariable=passlen).pack(pady=3)
-Button(root, text="Generate", command=generate).pack(pady=7)
-Entry(root, textvariable=passwrd).pack(pady=3)
-Button(root, text="Copy to clipboard", command=copytoclipboard).pack()
-root.mainloop()
+Label(window, text="Password Generator", font="Courier 30 italic").pack()
+Label(window, text="made by calV", font="Courier 15 bold").pack()
+Label(window, text="Enter the number of places for your Password").pack(pady=3)
+Entry(window, textvariable=passlen).pack(pady=3)
+Button(window, text="Generate", command=generate).pack(pady=7)
+Entry(window, textvariable=passwrd).pack(pady=3)
+Button(window, text="Copy to clipboard", command=copytoclipboard).pack()
+window.mainloop()
 
